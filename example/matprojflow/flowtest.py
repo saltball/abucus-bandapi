4b6e65c14f7669015654a5ac6339f1624a9c0e85
saltball <yanbohan98@gmail.com>
2021-08-19 18:41:56 +0800

    Fix flowtest example. - Check exists. - Add Lock for simple multiple machines. - Add ssh_exception in case of network connection issues.

--- example/matprojflow/flowtest.py
+++ example/matprojflow/flowtest.py
@@ -8,6 +8,8 @@
 
 import pathlib
 
+import dpdispatcher
+import paramiko.ssh_exception
 from bandapi.flow.abacus.matproj import AbacusFlowFromMatProj
 
 ABACUS_COMMAND = r"/home/ubuntu/abacus-develop/build/abacus"
@@ -48,28 +50,41 @@ else:
 #     "mp-149"
 # ]
 
+while 1:
+    try:
+        for smalllist in taskid:
+            if (pathlib.Path(r"lock")/smalllist in list(pathlib.Path(r"lock").iterdir())):
+                continue
+            else:
+                (pathlib.Path(r"lock")/smalllist).touch()
+            flow = AbacusFlowFromMatProj(
+                API_KEY=API_KEY,
+                machine=machine,
+                resource=resource,
+                task_flow_list=[
+                    # "relax",
+                    "scf-charge",
+                    "nscf-band",
+                    # "band-data"
+                ],
+                task_content=[smalllist],
+                task_setup={
+                    "potential_name": "SG15",
+                    "dr2": 1.0e-6, # dr2 parameter of ABACUS, see its document.
+                    "kpointscope": 5, # kpoint density for scf calculation. This number will be used for the smallest one of the three density, other number will be calculated due to lattice vector.
+                    "ecutwfc": 70, # ecutwfc parameter of ABACUS, see its document
+                    "remote_command": ABACUS_COMMAND, # see above comment
+                    "kpathrange": 15, # kpath density of band-structure calculation (only).
+                    "flow_work_root": LOCAL_ROOT, # local path of flow
+                    "submission_check_period": 1, # check time of tasks.
+                    # "nstep": 10,
+                    "clean": False, # if False, file on remote server won't be delete by dpdispatcher after calculations.(For frequently stop and unstable network connection.)
+                    "kpointfix":True # if True, kpoint with will be fix as Gamma:[kpointscope,kpointscope,kpointscope]
+                }
+            )
 
-flow = AbacusFlowFromMatProj(
-    API_KEY=API_KEY,
-    machine=machine,
-    resource=resource,
-    task_flow_list=[
-        "scf-charge",
-        "nscf-band",
-        # "band-data"
-    ],
-    task_content=taskid,
-    task_setup={
-        "potential_name": "SG15",
-        "dr2": 1.0e-7,
-        "kpointscope": 5,
-        "ecutwfc": 80,
-        "remote_command": ABACUS_COMMAND,
-        "kpathrange": 20,
-        "flow_work_root": LOCAL_ROOT,
-        "submission_check_period": 1,
-        "nstep": 10
-    }
-)
+            flow.submit()
+            (pathlib.Path(r"lock") / smalllist).unlink()
+    except paramiko.ssh_exception.SSHException:
+        pass
 
-flow.submit()
